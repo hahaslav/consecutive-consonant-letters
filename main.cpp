0f69bdba6f89c9f85212113e6c8df4b26f78b7e8
hahaslav <yara-2004@ukr.net>
2024-03-13 10:19:44 +0200

    Read file content

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+std::string read_file(std::string filename) {
+    std::ifstream fin(filename);
+    std::stringstream buffer;
+    buffer << fin.rdbuf();
+    fin.close();
+
+    return buffer.str();
+}
 
 int main() {
+    std::cout << read_file("input.txt") << "\n";
 
     return 0;
 }
